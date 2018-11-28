d947f28b76337ad6511f73b8d681e2bd2f739609
nejigram <nejigram@gmail.com>
2018-11-29 03:59:59 +0900

    all

---
+++ imgmove.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(window).load(function(){
+        $("#ibox01").height($("#ibox01").children("img").height());
+        $("#ibox02").height($("#ibox02").children("img").height());
+        $("#ibox03").height($("#ibox03").children("img").height());
+
+    });
+});
