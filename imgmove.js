449d410e6421d5ca40c1d06817dc75c356578b42
nejigram <nejigram@gmail.com>
2018-11-30 17:05:22 +0900

    jquery -> js

--- imgmove.js
+++ imgmove.js
@@ -1,8 +1,6 @@
-$(function(){
-    $(window).load(function(){
-        $("#ibox01").height($("#ibox01").children("img").height());
-        $("#ibox02").height($("#ibox02").children("img").height());
-        $("#ibox03").height($("#ibox03").children("img").height());
+window.onload = function(){
+  document.getElementById("ibox01").style.height = document.getElementById("ibox01").children[0].clientHeight + "px";
+  document.getElementById("ibox02").style.height = document.getElementById("ibox02").children[0].clientHeight + "px";
+  document.getElementById("ibox03").style.height = document.getElementById("ibox03").children[0].clientHeight + "px";
 
-    });
-});
+}
